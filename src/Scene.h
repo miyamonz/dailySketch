a96111562d7d279a69fbc2a6287140b48f23dbd7
Koichi Miyamoto <m5813k@gmail.com>
2017-12-11 22:08:25 +0900

    new Scene.h

--- src/Scene.h
+++ src/Scene.h
@@ -1,13 +1,60 @@
-//
-//  Scene.h
-//  dailySketch
-//
-//  Created by 宮本浩市 on 2017/12/11.
-//
-//
+#pragma once
+class Scene;
+using SceneRef = shared_ptr<Scene>;
 
-#ifndef Scene_h
-#define Scene_h
+class Scene {
+public:
+    float time;
+    Scene() : time(0)
+    {}
+    
+    using SceneRef = shared_ptr<Scene>;
+    vector<SceneRef> children;
+    
+    //add child
+    template<typename S, typename... Args>
+    shared_ptr<S> add(Args... args) {
+        auto scene = make_shared<S>(args...);
+        add(scene);
+        return scene;
+    }
+    void add(SceneRef scene) {
+        scene->setup();
+        children.emplace_back(scene);
+    }
+    
+    // setup update draw
+    virtual void baseSetup() { time = 0; setup(); }
+    virtual void setup() {}
+    
+    virtual void baseUpdate() {
+        time += ofGetLastFrameTime();
+        for(auto child : children) child->baseUpdate();
+        ofRemove(children, [](SceneRef scene){ return scene->isEnd(); });
+        children.shrink_to_fit();
+        update();
+    }
+    virtual void baseDraw() {
+        for(auto child : children) child->baseDraw();
+        draw();
+    }
+    virtual void update(){};
+    virtual void draw(){}
+    
+    //end
+    bool bEnd = false;
+    void end() {
+        bEnd = true;
+    }
+    virtual bool isEnd() {
+        if(children.size() == 0) return bEnd;
+        
+        if(bEnd) return true;
+        for(auto child : children) {
+            if(!child->isEnd()) return false;
+        }
+        return true;
+    }
+};
 
-
-#endif /* Scene_h */
+using SceneRef = shared_ptr<Scene>;
