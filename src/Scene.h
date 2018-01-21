67f2fb3d9d868347886281b6b0a191fb86d9aa34
Koichi Miyamoto <m5813k@gmail.com>
2018-01-21 23:14:40 +0900

    indent

--- src/Scene.h
+++ src/Scene.h
@@ -2,165 +2,165 @@
 
 namespace ofx {
     
-struct Component;
-using ComponentRef = shared_ptr<Component>;
-
-struct Component : public ofBaseApp {
-    string name;
-    float time;
-    Component() : time(0)
-    {}
+    struct Component;
+    using ComponentRef = shared_ptr<Component>;
     
-    static ComponentRef createRoot();
-    
-    Component* parent = nullptr;
-    vector<ComponentRef> children;
-    
-    //add child
-    template<typename S = Component, typename... Args>
-    shared_ptr<S> add(Args... args) {
-        auto com = make_shared<S>(args...);
-        add((ComponentRef)com);
-        return com;
-    }
-    void add(ComponentRef com) {
-        children.emplace_back(com);
-        com->parent = this;
-    }
-    vector<ComponentRef> addVec;
-    template<typename S = Component, typename... Args>
-    shared_ptr<S> addAfter(Args... args) {
-        auto com = make_shared<S>(args...);
-        addAfter((ComponentRef)com);
-        return com;
-    }
-    void addAfter(ComponentRef com) {
-        addVec.emplace_back(com);
-        com->parent = this;
-    }
-    
-    //kaisou
-    template<class Fn, typename... Args>
-    void _repeat(Fn fn, Args... args) {
-        fn(*this, args...);
-        for(auto&& c : children) c->_repeat<Fn,Args...>(fn, args...);
-    }
-    
-    template<class... Args>
-    using FP = void(Component::*)(Args...);
-
-    template<class... Args, class FP_ = FP<Args...>>
-     void recursiveCall(FP_ ptr, Args... args) {
-        auto fn = std::bind(ptr, this, args...);
-        fn(args...);
-        for(auto&& c : children) c->recursiveCall(ptr, args...);
-    }
-    
-    template<class... Args, class FP_ = FP<Args...>>
-    void recursiveCall(array<FP_,2> ptrs, Args... args) {
-        auto fn0 = std::bind(ptrs[0], this, args...);
-        auto fn1 = std::bind(ptrs[1], this, args...);
+    struct Component : public ofBaseApp {
+        string name;
+        float time;
+        Component() : time(0)
+        {}
         
-        fn0(args...);
-        for(auto&& c : children) c->recursiveCall(ptrs, args...);
-        fn1(args...);
-    }
-    
-    template<class... Args, class FP_ = FP<Args...>>
-    void callChildren(FP_ ptr, Args... args) {
-        for(auto&& c : children) {
-            if(c.get() == nullptr) continue;
-            auto fn = std::bind(ptr, c.get(), args...);
+        static ComponentRef createRoot();
+        
+        Component* parent = nullptr;
+        vector<ComponentRef> children;
+        
+        //add child
+        template<typename S = Component, typename... Args>
+        shared_ptr<S> add(Args... args) {
+            auto com = make_shared<S>(args...);
+            add((ComponentRef)com);
+            return com;
+        }
+        void add(ComponentRef com) {
+            children.emplace_back(com);
+            com->parent = this;
+        }
+        vector<ComponentRef> addVec;
+        template<typename S = Component, typename... Args>
+        shared_ptr<S> addAfter(Args... args) {
+            auto com = make_shared<S>(args...);
+            addAfter((ComponentRef)com);
+            return com;
+        }
+        void addAfter(ComponentRef com) {
+            addVec.emplace_back(com);
+            com->parent = this;
+        }
+        
+        //kaisou
+        template<class Fn, typename... Args>
+        void _repeat(Fn fn, Args... args) {
+            fn(*this, args...);
+            for(auto&& c : children) c->_repeat<Fn,Args...>(fn, args...);
+        }
+        
+        template<class... Args>
+        using FP = void(Component::*)(Args...);
+        
+        template<class... Args, class FP_ = FP<Args...>>
+        void recursiveCall(FP_ ptr, Args... args) {
+            auto fn = std::bind(ptr, this, args...);
             fn(args...);
-        }
-    }
-    virtual void setup() { time = 0; }
-    virtual void updateTime() {
-        time += ofGetLastFrameTime();
-    }
-    virtual void removeChild() {
-        ofRemove(children, [](ComponentRef com){ return com->isDone(); });
-        children.shrink_to_fit();
-    }
-    virtual void setupAll()  {
-        setup();
-        callChildren(&Component::setupAll);
-    }
-    void _moveChild() {
-        if(addVec.size() > 0) {
-            copy(addVec.begin(), addVec.end(), back_inserter(children));
-            addVec.clear();
+            for(auto&& c : children) c->recursiveCall(ptr, args...);
         }
         
-    }
-    virtual void updateAll() {
-        updateTime();
-        update();
-        _moveChild();
-        removeChild();
-        callChildren(&Component::updateAll);
+        template<class... Args, class FP_ = FP<Args...>>
+        void recursiveCall(array<FP_,2> ptrs, Args... args) {
+            auto fn0 = std::bind(ptrs[0], this, args...);
+            auto fn1 = std::bind(ptrs[1], this, args...);
+            
+            fn0(args...);
+            for(auto&& c : children) c->recursiveCall(ptrs, args...);
+            fn1(args...);
+        }
+        
+        template<class... Args, class FP_ = FP<Args...>>
+        void callChildren(FP_ ptr, Args... args) {
+            for(auto&& c : children) {
+                if(c.get() == nullptr) continue;
+                auto fn = std::bind(ptr, c.get(), args...);
+                fn(args...);
+            }
+        }
+        virtual void setup() { time = 0; }
+        virtual void updateTime() {
+            time += ofGetLastFrameTime();
+        }
+        virtual void removeChild() {
+            ofRemove(children, [](ComponentRef com){ return com->isDone(); });
+            children.shrink_to_fit();
+        }
+        virtual void setupAll()  {
+            setup();
+            callChildren(&Component::setupAll);
+        }
+        void _moveChild() {
+            if(addVec.size() > 0) {
+                copy(addVec.begin(), addVec.end(), back_inserter(children));
+                addVec.clear();
+            }
+            
+        }
+        virtual void updateAll() {
+            updateTime();
+            update();
+            _moveChild();
+            removeChild();
+            callChildren(&Component::updateAll);
+        }
+        
+        virtual void beforeDraw() {}
+        virtual void afterDraw() {}
+        virtual void drawAll() {
+            beforeDraw();
+            draw();
+            callChildren(&Component::drawAll);
+            afterDraw();
+        }
+        virtual void keyPressedAll(int key) {
+            keyPressed(key);
+            callChildren(&Component::keyPressedAll, key);
+        }
+        virtual void mousePressedAll(int x, int y, int button) {
+            mousePressed(x, y, button);
+            callChildren(&Component::mousePressedAll, x, y, button);
+        }
+        virtual void mouseReleasedAll(int x, int y, int button) {
+            mouseReleased(x, y, button);
+            callChildren(&Component::mouseReleasedAll, x, y, button);
+        }
+        virtual void mouseDraggedAll(int x, int y, int button) {
+            mouseDragged(x, y, button);
+            callChildren(&Component::mouseDraggedAll, x, y, button);
+        }
+        
+        bool bDone = false;
+        virtual bool isDone() {
+            return bDone;
+        }
+    };
+    
+    ComponentRef Component::createRoot() {
+        auto com = make_shared<Component>();
+        com->name = "root";
+        return com;
     }
     
-    virtual void beforeDraw() {}
-    virtual void afterDraw() {}
-    virtual void drawAll() {
-        beforeDraw();
-        draw();
-        callChildren(&Component::drawAll);
-        afterDraw();
-    }
-    virtual void keyPressedAll(int key) {
-        keyPressed(key);
-        callChildren(&Component::keyPressedAll, key);
-    }
-    virtual void mousePressedAll(int x, int y, int button) {
-        mousePressed(x, y, button);
-        callChildren(&Component::mousePressedAll, x, y, button);
-    }
-    virtual void mouseReleasedAll(int x, int y, int button) {
-        mouseReleased(x, y, button);
-        callChildren(&Component::mouseReleasedAll, x, y, button);
-    }
-    virtual void mouseDraggedAll(int x, int y, int button) {
-        mouseDragged(x, y, button);
-        callChildren(&Component::mouseDraggedAll, x, y, button);
-    }
+    struct ComponentSerialBase : virtual public Component {
+    private:
+        int idx = 0;
+    public:
+        ComponentRef getCurrent() {
+            return children.at(idx);
+        }
+        
+        void updateAll()                            { if(children.size() == 0) return; getCurrent()->updateAll(); }
+        void drawAll()                              { if(children.size() == 0) return; getCurrent()->drawAll();   }
+        void keyPressed(int key)                    { if(children.size() == 0) return; getCurrent()->keyPressed(key); }
+        void mousePressed(int x, int y, int button) { if(children.size() == 0) return; getCurrent()->mousePressed(x, y, button); }
+        void mouseDragged(int x, int y, int button) { if(children.size() == 0) return; getCurrent()->mouseDragged(x, y, button); }
+        
+        void setIndex(int i) {
+            idx = ofClamp(i, 0, children.size()-1);
+            getCurrent()->setupAll();
+        }
+        int getIndex() { return idx; }
+        void next() { setIndex(idx+1); }
+        void prev() { setIndex(idx-1); }
+    };
     
-    bool bDone = false;
-    virtual bool isDone() {
-        return bDone;
-    }
-};
-
-ComponentRef Component::createRoot() {
-    auto com = make_shared<Component>();
-    com->name = "root";
-    return com;
-}
-    
-struct ComponentSerialBase : virtual public Component {
-private:
-    int idx = 0;
-public:
-    ComponentRef getCurrent() {
-        return children.at(idx);
-    }
-    
-    void updateAll()                            { if(children.size() == 0) return; getCurrent()->updateAll(); }
-    void drawAll()                              { if(children.size() == 0) return; getCurrent()->drawAll();   }
-    void keyPressed(int key)                    { if(children.size() == 0) return; getCurrent()->keyPressed(key); }
-    void mousePressed(int x, int y, int button) { if(children.size() == 0) return; getCurrent()->mousePressed(x, y, button); }
-    void mouseDragged(int x, int y, int button) { if(children.size() == 0) return; getCurrent()->mouseDragged(x, y, button); }
-    
-    void setIndex(int i) {
-        idx = ofClamp(i, 0, children.size()-1);
-        getCurrent()->setupAll();
-    }
-    int getIndex() { return idx; }
-    void next() { setIndex(idx+1); }
-    void prev() { setIndex(idx-1); }
-};
-
     struct ComponentSerial : virtual public ComponentSerialBase {
         void update() {
             auto done = getCurrent()->bDone;
@@ -196,12 +196,12 @@ public:
 //    void baseDraw() override {
 //        if(idx >= children.size()) return;
 //        getCurrent()->baseDraw();
-//        
+//
 //        if(getCurrent()->isEnd() && bAutoPlay) next();
 //            }
 //    void checkIdx() {
 //        if(idx < children.size()) return;
-//        
+//
 //        if(bRepeat) {
 //            idx = 0;
 //            for(auto& c : children) c->baseSetup();
